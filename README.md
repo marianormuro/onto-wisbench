59f4cb4b93698cbbd5d661fd277ee2ec71a1eab4
Guohui Xiao <gh.xiao@gmail.com>
2013-08-19 19:07:49 +0200

    update docs

--- README.md
+++ README.md
@@ -1,17 +1,61 @@
 onto-wisbench
 =============
 
+Compile onto-wisbench 
+---------------------
+
+The project is organized by maven. You can compile it by
+
+```
+$ mvn package
+```
+
+
 create tables
 -------------
+
+* MySQL: 
+
+
+```
+CREATE SCHEMA `ontowisbench` ;
+```
+
+```
+
+
+$ echo sql/create.sql | mysql
+
+```
 
 generate data
 -------------
 
+```
+$ java -cp ontowisbench-1.0.jar generate 10000 > 10000.csv 
+```
+
 load data
 ---------
+
+* MySQL
+
+```
+$ /usr/local/mysql/bin/mysql -u root -p -d ontowisbench
+mysql> load data local infile '10000.csv' into table t1 fields terminated by ',';
+```
 
 index
 -----
 
+* MySQL
+
+```
+$ echo sql/index.sql | mysql
+```
+
 run benchmark
 -------------
+
+
+
---
+++ src/main/resources/index.sql
@@ -0,0 +1,29 @@
+ALTER TABLE `t`  ADD INDEX `two_idx` (`two` ASC);
+
+ALTER TABLE `t`  ADD INDEX `four_idx` (`four` ASC);
+
+ALTER TABLE `t`  ADD INDEX `ten_idx` (`ten` ASC);
+
+ALTER TABLE `t`  ADD INDEX `twenty_idx` (`twenty` ASC);
+
+ALTER TABLE `t`  ADD INDEX `onePercent_idx` (`onePercent` ASC);
+	
+ALTER TABLE `t`  ADD INDEX `tenPercent_idx` (`tenPercent` ASC); 
+
+ALTER TABLE `t`  ADD INDEX `twentyPercent_idx` (`twentyPercent` ASC);
+	
+ALTER TABLE `t`  ADD INDEX `fiftyPercent_idx` (`fiftyPercent` ASC);
+
+ALTER TABLE `t`  ADD INDEX `evenOnePercent_idx` (`evenOnePercent` ASC);
+
+ALTER TABLE `t`  ADD INDEX `oddOnePercent_idx` (`oddOnePercent` ASC);
+
+ALTER TABLE `t`  ADD INDEX `stringu1_idx` (`stringu1` ASC);
+
+ALTER TABLE `t`  ADD INDEX `stringu2_idx` (`stringu2` ASC);
+
+ALTER TABLE `t`  ADD INDEX `string4_idx` (`string4` ASC);
+
+ALTER TABLE `t`  ADD UNIQUE INDEX `unique1_unique_idx` (`unique1` ASC);
+
+ALTER TABLE `t`  ADD UNIQUE INDEX `unique3_unique_idx` (`unique3` ASC);
